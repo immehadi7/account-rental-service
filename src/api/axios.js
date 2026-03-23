0ed0dfc6f98f0196e2683e0b67a0eb6bd61d3264
immehadi7 <mehedy.adi7@gmail.com>
2026-03-23 11:12:18 +0600

    update api url

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,11 @@ const API = axios.create({
   baseURL: 'http://localhost:5000/api',
   withCredentials: true,
 })
+const API = axios.create({ 
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api', 
+  withCredentials: true 
+})
+
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
