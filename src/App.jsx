10f7310665a7bb4728b50ffe54b1fc97fbabe471
immehadi7 <mehedy.adi7@gmail.com>
2026-03-24 11:57:10 +0600

    fix all filename cases

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import DetailModal from './pages/modals/DetailModal.jsx'
 import AlipayModal from './pages/modals/Alipaymodal.jsx'
 import LoginModal from './pages/modals/LoginModal'
 import OrdersPage from './pages/OrdersPage'
+import DetailModal from './pages/modals/DetailModal.jsx'
+
+
 
 export default function App() {
   const [page,         setPage]         = useState('home')
